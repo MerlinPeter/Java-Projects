b061dcf5cc227c78b2a7340fa8007da2d2889982
MerlinPeter <merlinahila@gmail.com>
2017-08-09 08:49:08 -0700

    from command line

--- Selenium/src/Check.java
+++ Selenium/src/Check.java
@@ -4,8 +4,7 @@ public class Check {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Selinium  first check");	
-		System.out.println("Selinium  first check");
+ 		System.out.println("Selinium  first check");
 	}
 
 }
